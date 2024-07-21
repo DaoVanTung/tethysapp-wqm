d6c1bf1c9aa6f30e83d09a35ae458738134d55f7
tungdv <mail.daovantugn@gmail.com>
2024-07-21 08:00:24 +0700

    Filter giao diện ds điểm quan trắc

--- tethysapp/wqm/controllers/api.py
+++ tethysapp/wqm/controllers/api.py
@@ -50,3 +50,25 @@ def get_license_water_exploitation_points(request, id):
     conn.close()
 
     return JsonResponse({'data': result_list})
+
+@controller(url='/api/monitoring_stations')
+def get_monitoring_stations(request):
+    conn = psycopg2.connect(dbname=DB_NAME, user=USERNAME, password=PASSWORD, host=HOST)
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT * FROM public.diem_quan_trac")
+
+    # Lấy tất cả các dòng kết quả
+    rows = cur.fetchall()
+
+    # Lấy tên các cột
+    colnames = [desc[0] for desc in cur.description]
+
+    # Chuyển đổi dữ liệu thành danh sách các từ điển
+    result_list = [dict(zip(colnames, row)) for row in rows]
+
+    # Đóng kết nối và cursor sau khi hoàn thành
+    cur.close()
+    conn.close()
+
+    return JsonResponse({'data': result_list})
