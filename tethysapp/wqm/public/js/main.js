3bd0d636808812cc8ba39aaacd0dac5a0b4fe468
tungdv <mail.daovantugn@gmail.com>
2024-07-25 11:48:52 +0700

    Thêm bản đồ cho trang dashboard

--- tethysapp/wqm/public/js/main.js
+++ tethysapp/wqm/public/js/main.js
@@ -41,69 +41,74 @@ function create_map(map_container) {
     });
 
     map.addControl(new maplibregl.NavigationControl());
-
-    map.on("load", async function () {
-        const imagePromises = [
-            map.loadImage("/static/wqm/images/diem_quan_trac.png"),
-            map.loadImage("/static/wqm/images/diem_khai_thac.png"),
-
-        ];
-
-        Promise.all(imagePromises).then(images => {
-            map.addImage("diem_quan_trac_icon", images[0].data);
-            map.addImage("diem_khai_thac_icon", images[1].data);
-
-            map.addLayer(
-                {
-                    id: "diem_khai_thac_layer",
-                    type: "symbol",
-                    source: "diem_khai_thac",
-                    "source-layer": "table.public.diem_khai_thac_nuoc.geom",
-                    layout: {
-                        "icon-image": "diem_khai_thac_icon",
-                        "icon-size": 0.4,
-                    },
-
-                    filter: [">=", ["to-number", ["get", "luu_luong_khai_thac"]], 3000]
-                }
-            );
-
-            map.addLayer(
-                {
-                    id: "diem_quan_trac_layer",
-                    type: "symbol",
-                    source: "diem_quan_trac",
-                    "source-layer": "table.public.diem_quan_trac.geom",
-                    layout: {
-                        "icon-image": "diem_quan_trac_icon",
-                        "icon-size": 0.4,
-                    },
-                }
-            );
-
-            map.on("mouseenter", "diem_quan_trac_layer", function () {
-                map.getCanvas().style.cursor = "pointer";
-            });
-
-            map.on("mouseleave", "diem_quan_trac_layer", function () {
-                map.getCanvas().style.cursor = "";
-            });
-
-            // Thay đổi con trỏ chuột khi di chuyển qua điểm
-            map.on("mouseenter", "diem_khai_thac_layer", function () {
-                map.getCanvas().style.cursor = "pointer";
-            });
-
-            // Thay đổi con trỏ chuột về lại ban đầu khi rời khỏi điểm
-            map.on("mouseleave", "diem_khai_thac_layer", function () {
-                map.getCanvas().style.cursor = "";
-            });
-        });
-    });
-
     return map;
 }
 
 function hide_layer(layer_id) {
     map.setLayoutProperty(layer_id, 'visibility', 'none');
+}
+
+function add_ms_layer(map) {
+
+    const imagePromises = [
+        map.loadImage("/static/wqm/images/diem_quan_trac.png"),
+    ];
+
+    Promise.all(imagePromises).then(images => {
+        map.addImage("diem_quan_trac_icon", images[0].data);
+
+        map.addLayer(
+            {
+                id: "diem_quan_trac_layer",
+                type: "symbol",
+                source: "diem_quan_trac",
+                "source-layer": "table.public.diem_quan_trac.geom",
+                layout: {
+                    "icon-image": "diem_quan_trac_icon",
+                    "icon-size": 0.4,
+                },
+            }
+        );
+
+        map.on("mouseenter", "diem_quan_trac_layer", function () {
+            map.getCanvas().style.cursor = "pointer";
+        });
+
+        map.on("mouseleave", "diem_quan_trac_layer", function () {
+            map.getCanvas().style.cursor = "";
+        });
+    });
+}
+
+
+function add_water_point_layer(map) {
+
+    const imagePromises = [
+        map.loadImage("/static/wqm/images/diem_khai_thac.png"),
+    ];
+
+    Promise.all(imagePromises).then(images => {
+        map.addImage("diem_khai_thac_icon", images[0].data);
+
+        map.addLayer(
+            {
+                id: "diem_khai_thac_layer",
+                type: "symbol",
+                source: "diem_khai_thac",
+                "source-layer": "table.public.diem_khai_thac_nuoc.geom",
+                layout: {
+                    "icon-image": "diem_khai_thac_icon",
+                    "icon-size": 1,
+                },
+            }
+        );
+
+        map.on("mouseenter", "diem_khai_thac_layer", function () {
+            map.getCanvas().style.cursor = "pointer";
+        });
+
+        map.on("mouseleave", "diem_khai_thac_layer", function () {
+            map.getCanvas().style.cursor = "";
+        });
+    });
 }
---
+++ tethysapp/wqm/public/js/pages/dashboard.js
@@ -0,0 +1,2 @@
+let map = create_map('map');
+add_water_point_layer(map);
