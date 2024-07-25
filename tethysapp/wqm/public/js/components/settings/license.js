e485034657e2d0f7e5f420a1cbc084c5c3561782
tungdv <mail.daovantugn@gmail.com>
2024-07-25 14:36:12 +0700

    Bảng danh sách giấy phép

---
+++ tethysapp/wqm/public/js/components/settings/license.js
@@ -0,0 +1,141 @@
+let today = new Date();
+let next_30_day = new Date();
+next_30_day.setDate(today.getDate() + 30);
+
+function analysis_licenses() {
+    license_cache.forEach(element => {
+        let end_date = new Date(Date.parse(element['ngay_het_han']));
+
+        if (end_date < today) {
+            license_expired += 1;
+        } else if (end_date > today && end_date < next_30_day) {
+            license_expiring_soon += 1;
+        } else {
+            license_valid += 1;
+        }
+    });
+
+    $('#license-total').text(license_total.toLocaleString());
+    $('#license-valid').text(license_valid.toLocaleString());
+    $('#license-expiring-soon').text(license_expiring_soon.toLocaleString());
+    $('#license-expired').text(license_expired.toLocaleString());
+}
+
+
+function fill_licenses_to_table() {
+    let data = license_cache;
+
+    if (license_table !== null) {
+        license_table.destroy();
+        license_table = null;
+        $('#license-table tbody').off();
+    }
+
+    license_table = $('#license-table').DataTable({
+        data: data,
+        language: {
+            zeroRecords: 'Không có kết quả tìm kiếm phù hợp',
+            "lengthMenu": "Số giấy phép cho mỗi trang _MENU_",
+            "info": "Đang hiển thị _START_ đến _END_ của _TOTAL_ giấy phép",
+            "search": "Tìm kiếm",
+        },
+
+        searching: false,
+        pageLength: 25,
+        order: [[0, 'asc']],
+        columnDefs: [],
+        columns: [
+            {
+                "targets": 0,
+                title: "STT",
+                "render": function (data, type, full, meta) {
+                    return meta.row + 1;
+                }
+            },
+            {
+                data: "so_hieu_van_ban",
+                title: "Số hiệu văn bản",
+                width: '180px',
+                render: (data, type, row) => {
+                    return data;
+                },
+            },
+            {
+                data: "ngay_ban_hanh",
+                title: "Ngày ban hành",
+                width: '150px',
+                render: (data, type, row) => {
+                    if (data == null) return "";
+                    let datetime = new Date(Date.parse(data));
+                    return datetime.toLocaleDateString("vi-VN");
+                },
+            },
+            {
+                data: "ngay_het_han",
+                title: "Ngày hết hạn",
+                width: '150px',
+                render: (data, type, row) => {
+                    if (data == null) return "";
+                    let end_date = new Date(Date.parse(data));
+
+                    if (end_date < today) {
+                        return `
+                        <span style="color: #dc3545;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-excel-fill" viewBox="0 0 16 16">
+                        <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M5.884 6.68 8 9.219l2.116-2.54a.5.5 0 1 1 .768.641L8.651 10l2.233 2.68a.5.5 0 0 1-.768.64L8 10.781l-2.116 2.54a.5.5 0 0 1-.768-.641L7.349 10 5.116 7.32a.5.5 0 1 1 .768-.64"/>
+                        </svg>
+                        ${end_date.toLocaleDateString("vi-VN")}
+                        
+                        </span>`;
+                    } else if (end_date > today && end_date < next_30_day) {
+                        return `<span style="color: #ffc107;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+                        </svg>
+                        ${end_date.toLocaleDateString("vi-VN")}
+                        </span>`;
+                    } else {
+                        return `<span style="color: green;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-check-fill" viewBox="0 0 16 16">
+                        <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1m1.354 4.354-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708"/>
+                        </svg>
+                        ${end_date.toLocaleDateString("vi-VN")}
+                        </span>`;
+                    }
+                },
+            },
+            {
+                data: "loai_giay_phep",
+                title: "Loại giấy phép",
+                width: '280px',
+                render: (data, type, row) => {
+                    if (data == null) return "";
+                    return data;
+                },
+            },
+            {
+                data: "ten_to_chuc_ca_nhan",
+                title: "Tên tổ chức/cá nhân",
+                render: (data, type, row) => {
+                    return data;
+                },
+            },
+            {
+                data: "id",
+                title: "",
+                width: '100px',
+                searchable: false,
+                orderable: false,
+                render: (data, type, row) => {
+                    return `
+                    <div class='d-flex' style="justify-content: center;">
+                        <button type="button" class="btn btn-link" onclick="on_view_license_button_click('${data}')">Chi tiết</button>
+                    </div>
+                    `;
+                },
+            },
+        ]
+    });
+
+    // addSearchResultEvent();
+}
---
+++ tethysapp/wqm/public/js/pages/settings.js
@@ -0,0 +1,73 @@
+// Lấy danh sách giấy phép
+var license_cache = [];
+var license_table = null;
+
+//  Tổng giấy phép
+var license_total = 0;
+
+// Giấy phép còn hạn
+var license_valid = 0;
+
+// Giấy phép sắp hết hạn
+var license_expiring_soon = 0;
+
+// Giấy phép đã hết hạn
+var license_expired = 0;
+
+// Lấy danh sách điểm quan trắc
+var monitor_station_cache = [];
+var monitor_station_table = null;
+
+$('.menu-box__item').on('click', function () {
+    show_content($(this).attr('id'));
+});
+
+
+function get_license_data() {
+    $.ajax({
+        'url': '/apps/wqm/api/licenses/',
+        'method': 'GET',
+        'success': function (res) {
+            license_cache = res['data'];
+            license_total = license_cache.length;
+    
+            fill_licenses_to_table();
+            analysis_licenses();
+        }
+    });
+}
+
+function get_ms_data() {
+    $.ajax({
+        'url': '/apps/wqm/api/monitoring_stations/',
+        'method': 'GET',
+        'success': function (res) {
+            monitor_station_cache = res['data'];
+            fill_monitor_station_to_table();
+        }
+    });    
+}
+
+function show_content(content_id) {
+    let menu_item_selected = $('.menu-box__item.active').attr('id');
+
+    if (content_id === menu_item_selected) {
+        return;
+    }
+
+    if (content_id === 'menu-station' && station_cache.length == 0) {
+        get_ms_data();
+    }
+
+    if (content_id === 'menu-station' && station_cache.length == 0) {
+        get_ms_data();
+    }
+
+    $('.menu-box__item.active').removeClass('active');
+    $('.menu-box__item.active').removeClass('active');
+    $(`#content-${menu_item_selected}`).addClass('d-none');
+    $(`#content-${content_id}`).removeClass('d-none');
+    $(`#${content_id}`).addClass('active');
+}
+
+get_license_data();
